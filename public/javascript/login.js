63a3fd4e19a7116661ce8a4a83e2b7995b27870d
joe <hudakjoe@msn.com>
2022-03-31 20:49:29 -0400

    added a seperate page to login sign up then edited the handlebars to match

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 async function loginFormHandler(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim()
-  const password = document.querySelector('#password-login').value.trim()
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
@@ -12,46 +12,16 @@ async function loginFormHandler(event) {
         password,
       }),
       headers: { 'Content-Type': 'application/json' },
-    })
+    });
 
     if (response.ok) {
-      document.location.replace('/dashboard')
+      document.location.replace('/dashboard');
     } else {
-      alert(response.statusText)
-    }
-  }
-}
-
-async function signupFormHandler(event) {
-  event.preventDefault()
-
-  const username = document.querySelector('#username-signup').value.trim()
-  const email = document.querySelector('#email-signup').value.trim()
-  const password = document.querySelector('#password-signup').value.trim()
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-
-    if (response.ok) {
-      document.location.replace('/')
-    } else {
-      alert(response.statusText)
+      alert(response.statusText);
     }
   }
 }
 
 document
   .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler)
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler)
+  .addEventListener('submit', loginFormHandler);
