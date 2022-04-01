77bb67a05ab2a49b7c8adcea44336b4b1c6f6d46
joe <hudakjoe@msn.com>
2022-04-01 17:46:37 -0400

    working on getting them data bases to work

--- models/index.js
+++ models/index.js
@@ -3,35 +3,35 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-  foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'cascade',
-  hooks: true,
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'cascade',
-  hooks: true,
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'cascade',
-  hooks: true,
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'cascade',
-  hooks: true,
-});
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
+})
 
 // Export the modules
 module.exports = { User, Post, Comment };
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,29 +1,27 @@
 async function signupFormHandler(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/user', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-      alert('Account created! Logging you in now.');
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+            alert('Account created! Logging you in now.');
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText)
+        }
     }
-  }
 }
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
